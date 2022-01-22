10aef4f4e6b4f54e21752ea03f33d41ca0fbd766
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-22 15:32:22 +0300

    search: fix test cases.

--- search/binary_test.go
+++ search/binary_test.go
@@ -11,8 +11,8 @@ func TestBinary(t *testing.T) {
 		arr          []int
 		target, want int
 	}{
-		{[]int{0, 20, 80, 30, 60, 50, 110, 100, 130, 170}, 110, 6},
-		{[]int{0, 20, 80, 30, 60, 50, 110, 100, 130, 170}, 200, -1},
+		{[]int{0, 20, 30, 50, 60, 70, 80, 100, 130, 170}, 110, 6},
+		{[]int{0, 20, 30, 50, 60, 70, 80, 100, 130, 170}, 200, -1},
 	}
 	for _, test := range tests {
 		got := search.Binary(test.arr, test.target, test.arr[0], len(test.arr)-1)
