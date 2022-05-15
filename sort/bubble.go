0d5926a9132b649701417d5ee442d6d07c233a26
Yousif Sabah <iyousifsabah@gmail.com>
2022-05-15 14:07:57 +0300

    sort: update bubble sort algorithm with Generics.

--- sort/bubble.go
+++ sort/bubble.go
@@ -1,13 +1,24 @@
 package sort
 
+type Number interface {
+  int32 | int64 | float32 | float64
+}
+
+
 // Bubble implementation of bubble sort algorithm.
-func Bubble (arr []int) []int {
-  for i := 0; i < len(arr) - 1; i++ {
-    for k := 0; k < len(arr) - 1 - i; k++ {
-      if arr[i] < arr[k + 1] {
-        arr[i], arr[k + 1] = arr[k + 1], arr[i]
+func Bubble[N Number] (arr []N) []N {
+  n := len(arr)
+  swapped := true
+  
+  for swapped {
+    swapped = false
+    for i := 0; i < n-1; i++ {
+      if arr[i] > arr[i+1] {
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swapped = true
       }
     }
   }
+  
   return arr
 }
